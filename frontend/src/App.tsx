8c143241961d12c129463fcd1b95fe049fb63595
kashmii <www.myac@gmail.com>
2023-06-18 23:25:30 +0900

    deviseの導入・ログイン確認

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react"
 
+// testAPIを呼び出すメソッドをimport
 import { execTest } from "lib/api/test"
 
 const App: React.FC = () => {
+  // useStateで変数messageを宣言
   const [message, setMessage] = useState<string>("")
 
+  // apiを叩いてレスポンスを変数に入れるメソッドを宣言
   const handleExecTest = async () => {
     const res = await execTest()
 
@@ -13,6 +16,7 @@ const App: React.FC = () => {
     }
   }
 
+  // 第2引数の値が変わったときのみ、第1引数の処理を実行する
   useEffect(() => {
     handleExecTest()
   }, [])
